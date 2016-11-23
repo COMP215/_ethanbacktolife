6a1f2b8ca192a5e159e6eb59546460640616036f
Ethan Powell <Ethan Powell>
2016-11-23 15:43:15 -0500

    subsequence generator tested and working

--- subsequence_generator/main.cpp
+++ subsequence_generator/main.cpp
@@ -4,30 +4,40 @@ using namespace std;
 
 int main()
 {
-   string sequence,
-          new_subsequence;
-   char next_char;
-   vector<string> all_subsequences,
-                  new_subsequences;
-   sequence = "BACBAD";
-   all_subsequences.push_back(sequence[0]);
-   for (short i = 1; i < sequence.length(); i++)
-   {
-       next_char = sequence[i];
 
-       for (short i = 0; i < all_subsequences.size(); i++)
+   string new_subsequence,
+          next_char;
+   vector<string> sequence,
+                  all_subsequences,
+                  new_subsequences;
+   sequence.push_back("B");
+   sequence.push_back("A");
+   sequence.push_back("C");
+   sequence.push_back("B");
+   sequence.push_back("A");
+   sequence.push_back("D");
+   new_subsequence = sequence.at(0);
+   all_subsequences.push_back(new_subsequence);
+   for (short i = 1; i < sequence.size(); i++)
+   {
+       next_char = sequence.at(i);
+
+       for (short j = 0; j < all_subsequences.size(); j++)
        {
-           new_subsequence = all_subsequences.at(i) + next_char;
+           new_subsequence = all_subsequences.at(j) + next_char;
            new_subsequences.push_back(new_subsequence);
        }
-       for (short i = 0; i < new_subsequences.size(); i++)
+       for (short j = 0; j < new_subsequences.size(); j++)
        {
-           all_subsequences.push_back(new_subsequences.at(i));
+           all_subsequences.push_back(new_subsequences.at(j));
        }
-       all_subsequences.push_back(i);
+       new_subsequences.clear();
+       new_subsequence = sequence.at(i);
+       all_subsequences.push_back(new_subsequence);
    }
    for (short i = 0; i < all_subsequences.size(); i++)
    {
        cout << all_subsequences.at(i) << endl;
    }
+   cout << "Maximum number of distinct subsequences: " << all_subsequences.size() << endl;
 }
